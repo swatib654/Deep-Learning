ae92deb8bf229eb32978608be6b5e9305aa81678
Swati Bhosale <swatikb0209@gmail.com>
2025-10-07 18:40:30 +0530

    Add files via upload

---
+++ ANN/code2.py
@@ -0,0 +1,95 @@
+# Artificial Neural Network
+
+# Part 1 - Data Preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(r"C:\Users\pc\Downloads\Churn_Modelling.csv")
+X = dataset.iloc[:, 3:13]
+y = dataset.iloc[:, 13]
+
+# Create dummy variables
+geography = pd.get_dummies(X["Geography"], drop_first=True)
+gender = pd.get_dummies(X["Gender"], drop_first=True)
+
+# Concatenate the Data Frames
+X = pd.concat([X, geography, gender], axis=1)
+
+# Drop Unnecessary columns
+X = X.drop(['Geography', 'Gender'], axis=1)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Building the ANN
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units=11, kernel_initializer='he_uniform', activation='relu', input_dim=11))
+# classifier.add(Dropout(0.3))
+
+# Adding the second hidden layer
+classifier.add(Dense(units=11, kernel_initializer='he_normal', activation='relu'))
+# classifier.add(Dropout(0.4))
+
+# Adding the third hidden layer
+classifier.add(Dense(units=15, kernel_initializer='he_normal', activation='relu'))
+# classifier.add(Dropout(0.2))
+
+# Adding the output layer
+classifier.add(Dense(units=1, kernel_initializer='glorot_uniform', activation='sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer='Adamax', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fitting the ANN to the Training set
+model_history = classifier.fit(X_train, y_train, validation_split=0.33, batch_size=10, epochs=10)
+# Note: 'epochs' replaces 'nb_epoch'
+
+# Part 3 - Evaluating the Model
+# summarize history for accuracy
+plt.plot(model_history.history['accuracy'])
+plt.plot(model_history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:\n", cm)
+
+# Calculate the Accuracy
+score = accuracy_score(y_test, y_pred)
+print("Accuracy:", score)
+
+
+#**********************************************************************
